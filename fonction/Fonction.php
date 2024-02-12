1dd79f29b233ffae9112bcd359a168c2075d2769
finiral <finiral@yahoo.fr>
2024-02-13 02:19:20 +0300

    mbola tsi mety ny restant

--- fonction/Fonction.php
+++ fonction/Fonction.php
@@ -120,7 +120,7 @@ function modifMvtDepense($iddepense,$idCateDepense,$montat,$date){
 function getPoidtotal($date1,$date2){
     $rep=0;
     $base=dbconnect();
-    $requete="select sum(Poids_Cueilli) as total from v_cueillette where Date_Cueillette>%s and Date_Cueillette<%s"
+    $requete="select sum(Poids_Cueilli) as total from v_cueillette where Date_Cueillette>%s and Date_Cueillette<%s";
     $requete=sprintf($requete,$date1,$date2);
     $result=mysqli_query($base,$requete);
     while($donnees=mysqli_fetch_assoc($result)){
@@ -130,11 +130,13 @@ function getPoidtotal($date1,$date2){
 }
 function getpoidrestant($date1,$date2){
     $poidtotal=getPoidtotal($date1,$date2);
-    $requete="select sum(Surface_Parcelle) as totalParcelle from Cueillette where Date_Cueillette>%s and Date_Cueillette<%s"
+    $requete="select sum(Surface_Parcelle) as totalParcelle from Cueillette where Date_Cueillette>%s and Date_Cueillette<%s";
+    $base=dbconnect();
     $requete=sprintf($requete,$date1,$date2);
     $result=mysqli_query($base,$requete);
     while($donnees=mysqli_fetch_assoc($result)){
         $rep=$donnees['totalParcelle'];
     }
+    return $rep-$poidtotal;
 }
 ?>
