6cd293d473cabac6214a37812830e053fc910dcb
finiral <finiral@yahoo.fr>
2024-02-12 21:36:23 +0300

    back-office front

---
+++ pages/admin/table_parcelle.php
@@ -0,0 +1,24 @@
+<br>
+<h2>Liste parcelle</h2>
+<div class="table-responsive small">
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <th scope="col">Numéro parcelle</th>
+        <th scope="col">Surface</th>
+        <th scope="col">Variété de thé</th>
+        <th scope="col">Modifier</th>
+        <th scope="col">Supprimer</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td scope="col">Test</td>
+        <td scope="col">Test</td>
+        <td scope="col">Test</td>
+        <td scope="col"><a href="">Edit</a></td>
+        <td scope="col"><a href="">Delete</a></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
---
+++ pages/admin/table_the.php
@@ -0,0 +1,24 @@
+<br>
+<h2>Liste variété</h2>
+<div class="table-responsive small">
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <th scope="col">Nom</th>
+        <th scope="col">Occupation</th>
+        <th scope="col">Rendement</th>
+        <th scope="col">Modifier</th>
+        <th scope="col">Supprimer</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td scope="col">Test</td>
+        <td scope="col">Test</td>
+        <td scope="col">Test</td>
+        <td scope="col"><a href="">Edit</a></td>
+        <td scope="col"><a href="">Delete</a></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
