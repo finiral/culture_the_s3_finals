35703875329f4e1660523410d91080cf7d1d9073
RohyfuturBillionaire <rohyrajoelina@gmail.com>
2024-02-13 02:15:02 +0300

    t

--- fonction/base.php
+++ fonction/base.php
@@ -3,8 +3,9 @@ function dbConnect()
 {
     static $connect=null;
     if($connect===null)
-    {   $connect=mysqli_connect("172.20.0.167","ETU002708","fj43gRCIMWTX","thecompany","3306");
-        //$connect=mysqli_connect("localhost","root","","thecompany","3306");
+
+    {   //$connect=mysqli_connect("172.20.0.167","ETU002708","fj43gRCIMWTX","thecompany","3306");
+        $connect=mysqli_connect("localhost","root","","thecompany","3306");
     }
     return $connect;
     
