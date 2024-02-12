f613ba2fea4856d2a0c35b2cb11bcec723e76a55
ismaelrb24 <Fyrabarison24@gmail.com>
2024-02-12 17:26:19 +0300

    Update Fonction.php

--- Fonction.php
+++ Fonction.php
@@ -1,11 +1,11 @@
 <?php 
 include "base.php";
-function checklogin($identifiant,$motdepasse){
+function checklogin($identifiant,$motdepasse,$type){
     $rep=false;
    // $mdp=sha1($motdepasse);
     $base=dbConnect();
-    $requete="select * from employes where Email='%s' and mdp='sha1(%s)'";
-    sprintf($requete,$identifiant,$motdepasse);
+    $requete="select * from User where Pseudo='%s' and Mdp='sha1(%s)' and  Type_User='%i'";
+    sprintf($requete,$identifiant,$motdepasse,$type);
     $result=mysqli_query($base,$requete);
     $isa=mysqli_num_rows($result);
     if($isa==1){
@@ -16,10 +16,10 @@ function checklogin($identifiant,$motdepasse){
     }
     return $rep;
 }
-function getuser($identifiant,$motdepasse){
+function getuser($identifiant,$motdepasse,$type){
     $base=dbconnect();
-    $requete="select * from employes where Email='%s' and mdp='sha1(%s)'";
-    sprintf($requete,$identifiant,$motdepasse);
+    $requete="select * from User where Pseudo='%s' and Mdp='sha1(%s)' and Type_User='%i'";
+    sprintf($requete,$identifiant,$motdepasse,$type);
     $result=mysqli_query($base,$requete);
      while($donnees=mysqli_fetch_assoc($result)){
          $rep=$donnees['id'];
@@ -41,7 +41,7 @@ function insertvariete($nom,$occupation,$rendement){
     $base=dbconnect();
     $requete="insert into The values(null,'%s','%o','%o')";
     sprintf($requete,$nom,$occupation,$rendement);
-    $result=mysqli_query($base,);
+    $result=mysqli_query($base,$requete);
 }
 function modifvariete($idthe,$nom,$occupation,$rendement){
     $base=dbconnect();
@@ -53,13 +53,13 @@ function supprimer($id,$table){
     $base=dbconnect();
     $requete="delete from %s where id_%s='%i'";
     sprintf($requete,$table,$table,$id);
-        $result=mysqli_query($base,$requete)
+        $result=mysqli_query($base,$requete);
 }
 function insertParcelle($surface,$idthe){
     $base=dbconnect();
     $requete="insert into Parcelle values(null,'%o','%i')";
     sprintf($requete,$surface,$idthe);
-    $result=mysqli_query($base,$requete)
+    $result=mysqli_query($base,$requete);
 }
 function modifParcelle($idparcelle,$surface,$idthe){
     $base=dbconnect();
@@ -71,7 +71,7 @@ function insertCueilleur($nom,$genre,$dtn){
     $base=dbconnect();
     $requete="insert into Cueilleur values(null,'%s','%s','%s')";
     sprintf($requete,$nom,$genre,$dtn);
-    $result=mysqli_query($base,$requete)
+    $result=mysqli_query($base,$requete);
 }
 function modifCueilleur($idcueilleur,$nom,$genre,$dtn){
     $base=dbconnect();
@@ -83,7 +83,7 @@ function insertCueillette($idcueilleur,$idparcelle,$date,$poid){
     $base=dbconnect();
     $requete="insert into Cueillette values(null,'%i','%i','%s','%o')";
     sprintf($requete,$idcueilleur,$idparcelle,$date,$poid);
-    $result=mysqli_query($base,$requete)
+    $result=mysqli_query($base,$requete);
 }
 function modifCueillette($idcueillette,$idcueilleur,$idparcelle,$date,$poid){
     $base=dbconnect();
@@ -97,7 +97,7 @@ function insertCateDepense($nom){
     $base=dbconnect();
     $requete="insert into CateDepense values(null,'%s')";
     sprintf($requete,$nom);
-    $result=mysqli_query($base,$requete)
+    $result=mysqli_query($base,$requete);
 }
 function modifCateDepense($idCateDepense,$nom){
     $base=dbconnect();
@@ -109,7 +109,7 @@ function insertMvtDepense($idCateDepense,$montat,$date){
     $base=dbconnect();
     $requete="insert into MvtDepense values(null,'%i','%o','%$')";
     sprintf($requete,$idCateDepense,$montat,$date);
-    $result=mysqli_query($base,$requete)
+    $result=mysqli_query($base,$requete);
 }
 function modifMvtDepense($iddepense,$idCateDepense,$montat,$date){
     $base=dbconnect();
