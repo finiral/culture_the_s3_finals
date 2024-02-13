43fb6bb5c45ccbd8aa653cbdd1902e88d2b8694f
finiral <finiral@yahoo.fr>
2024-02-13 06:06:58 +0300

    changed

--- pages/admin/form_parcelle.php
+++ pages/admin/form_parcelle.php
@@ -2,10 +2,6 @@
     <h2 class="mb-4">Insertion parcelle</h2>
     <form>
         <div class="mb-3">
-            <label for="numero" class="form-label">Numero parcelle</label>
-            <input type="number" class="form-control" id="numero" name="numero" required>
-        </div>
-        <div class="mb-3">
             <label for="surface" class="form-label">Surface (en hectar)</label>
             <input type="number" class="form-control" id="surface" name="surface" required>
         </div>
@@ -13,7 +9,6 @@
             <label for="the" class="form-label">Variété de thé planté</label>
             
             <select class="form-control" id="the" name="the" required>
-                <option value="">test</option>
                 <option value="">test</option>
                 <option value="">test</option>
             </select>
