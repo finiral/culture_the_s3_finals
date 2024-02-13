b0870ac3cdafe3b46564ea92c67b3390ae5d1c98
finiral <finiral@yahoo.fr>
2024-02-13 11:59:19 +0300

    integr

--- fonction/base.php
+++ fonction/base.php
@@ -5,8 +5,8 @@ function dbConnect()
     if($connect===null)
 
     {   
-        $connect=mysqli_connect("172.20.0.167","ETU002708","fj43gRCIMWTX","db_p16_ETU002708","3306");
-        //$connect=mysqli_connect("localhost","root","","thecompany","3306");
+        //$connect=mysqli_connect("172.20.0.167","ETU002708","fj43gRCIMWTX","db_p16_ETU002708","3306");
+        $connect=mysqli_connect("localhost","root","","thecompany","3306");
     }
     return $connect;
     
