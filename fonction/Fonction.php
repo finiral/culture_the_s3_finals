a00312540b05fae9b47c6ca4d7ab898f53ad8802
finiral <finiral@yahoo.fr>
2024-02-13 18:13:14 +0300

    E

--- fonction/Fonction.php
+++ fonction/Fonction.php
@@ -152,7 +152,7 @@ function getPoidtotal($date1,$date2){
 function getVentetotal($date1,$date2){
     $rep=0;
     $base=dbconnect();
-    $requete="select sum(PrixVente) as Vente from cueillette join Parcelle on Parcelle.id_Parcelle=Cueillette.id_Parcelle join The on The.id_The=Parcelle.id_The where Date_Cueillette>'%s' and Date_Cueillette<'%s'group by PrixVente;";
+    $requete="select sum(PrixVente) as Vente from Cueillette join Parcelle on Parcelle.id_Parcelle=Cueillette.id_Parcelle join The on The.id_The=Parcelle.id_The where Date_Cueillette>'%s' and Date_Cueillette<'%s'group by PrixVente;";
     $requete=sprintf($requete,$date1,$date2);
     $result=mysqli_query($base,$requete);
     if($donnees=mysqli_fetch_assoc($result)){
